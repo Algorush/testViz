c4fcaf25a1a19ab1537c23e2a380f833c7feaa1b
Alexander Goryushkin <aleotu@gmail.com>
2025-03-29 17:36:20 -0300

    change in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,12 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './src/index.html',
       filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/config.html',
+      filename: 'config.html',
     }),
     new CopyWebpackPlugin({
       patterns: [

765d3d1604b8265571716fe9d795f510baf29f44
Alexander Goryushkin <aleotu@gmail.com>
2025-03-30 19:33:49 -0300

    add react and globe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Globe from "./GlobeViz";
+
 window.onload = () => {
   tableau.extensions.initializeAsync({ configure: configure })
     .then(() => {

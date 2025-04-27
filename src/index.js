52a496e070662d7c1e61602a7e8fc44094f326e6
Alexander Goryushkin <aleotu@gmail.com>
2025-04-27 12:19:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -85,11 +85,11 @@ function processTableauData(dataTable) {
     const currentConfig = fullConfig[currentWorksheet.name] || {};   
 
     console.log("Current config:", currentConfig);
-    const latField = currentConfig.latitude && columns.find(colName => colName.match(/(L|l)atitude/));
-    const lonField = currentConfig.longitude && columns.find(colName => colName.match(/(L|l)ongitude/));
+    const latField = currentConfig.latitude || columns.find(colName => colName.match(/(L|l)atitude/));
+    const lonField = currentConfig.longitude || columns.find(colName => colName.match(/(L|l)ongitude/));
     const sizeField = currentConfig.size;
     const colorField = currentConfig.color;
-    const countryField = currentConfig.country && columns.find(colName => colName.match(/(C|c)ountry/));
+    const countryField = currentConfig.country || columns.find(colName => colName.match(/(C|c)ountry/));
 
     let configType = "";
 

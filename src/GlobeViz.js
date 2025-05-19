3c4e9bc99f9c4968208684e09e9890e1e0119376
Alexander Goryushkin <aleotu@gmail.com>
2025-05-19 14:06:00 -0400

    Update GlobeViz.js

--- src/GlobeViz.js
+++ src/GlobeViz.js
@@ -36,7 +36,7 @@ const GlobeViz = ({ data, config }) => {
       // Initialize globe
       var globe = new Globe(containerRef.current)
         .showAtmosphere(false)
-        .backgroundColor('#f5f5f5')
+        .backgroundColor('rgb(243,243,243)')
         .pointOfView({ lat: 30, lng: -90, altitude: config.defaultAltitude })
         .pointsData(data.filter(d => {
           const lat = parseFloat(d[config.latitude]);

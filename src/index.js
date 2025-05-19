2a7303800e53d5acd3ea85b3e81a0da359f7491a
Alexander Goryushkin <aleotu@gmail.com>
2025-05-19 16:40:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ export async function initExtension() {
       country: currentConfig.country || "Country",
       size: currentConfig.size || null,
       color: currentConfig.color || null,
-      oceanColor: currentConfig.oceanColor || 'black',
+      oceanColor: currentConfig.oceanColor || 'rgb(243,243,243)',
       hoverColor: currentConfig.hoverColor || '#FF0000',
       pointColor: currentConfig.pointColor || "#ff6200",
       pointRadius: parseFloat(currentConfig.pointRadius) || 0.3,
